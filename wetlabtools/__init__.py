b31bf74660fc4de8dbb04a2b0191a908581bd945
Nicolas <nicolas.goldbach@epfl.ch>
2023-09-13 16:42:11 +0200

    added __init__.py and added SEC-MALS plot function

---
+++ wetlabtools/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for wetlabtools"""
+
+from .plot import *
+
+__author__ = "Nicolas Goldbach"
+__email__ = "nicolas.goldbach@epfl.ch"
+__version__ = "0.0.1"
--- wetlabtools/plot.py
+++ wetlabtools/plot.py
@@ -249,7 +249,7 @@ def plot_CD(data:dict, zooms:list, out_path:str='.', cutoff:float=2.0, mode:str=
             sns.lineplot(data=dfn, legend="full", ax=ax1, dashes=False, palette="ch:s=-.2,r=.6").set(title=sample_name)
             
             if mode == 'FADE':
-                dff = df.mask(df_a <= cutoff-cutoff*0.1)
+                dff = df.mask(df_a <= cutoff+cutoff*0.1)
                 sns.lineplot(data=dff, legend=False, ax=ax1, dashes=False, palette="ch:s=-.2,r=.6", alpha=0.2)
                 
         sns.move_legend(ax1, "upper left", bbox_to_anchor=(1,1), prop={'size': 8}, ncol=2, title='Temperature [°C]', frameon=False)
@@ -319,4 +319,79 @@ def plot_CD(data:dict, zooms:list, out_path:str='.', cutoff:float=2.0, mode:str=
         plt.close()
         i = i + 1
     
+    return None
+
+
+
+# =======================================
+# SEC-MALS
+# =======================================
+def plot_MALS(path:str, flow_rate:float, min_x:float=0, max_x:float=999, save_pdf:bool=False, save_png:bool=False):
+    '''
+    :param: path: str, path to the directory with csv files
+    :param: flow_rate: float, flow rate in ml/min to convert min to ml (x-axis)
+    :param: min_x: float, minimum retention volume to plot
+    :param: max_x: float, maximum retention volume to plot
+    :param: save_png: bool, whether to save plots as pdf
+    :param: save_pdf: bool, whether to save plots as png
+    
+    Function to plot data from SEC-MALS. It will parse the directory for all csv files and plot them as SEC-MALS data.
+    '''
+
+    # collect all csv files in path
+    paths = []
+    for file in os.listdir(path):
+        if file.endswith('.csv'):
+            paths.append(os.path.join(path,file))
+    
+    # plot data for all csv files
+    for csv_path in paths:
+        sample_name = os.path.basename(csv_path).split('_')[-1].split('.')[0]
+
+        df = pd.read_csv(csv_path)
+        df.rename(columns = {df.columns[7]:'MW [Da]', df.columns[5]: "UV [Relative scale]"}, inplace = True)
+        
+        fig,ax = plt.subplots()
+        
+        # Set y-axis variable for left y-axis
+        ax.plot(df["time (min).1"] * flow_rate,
+                df ["UV [Relative scale]"],
+                color='#1f77b4',
+                linewidth = 0.8)
+        ax.set_ylabel(ylabel = "UV [Relative scale]",
+                color='#1f77b4',
+                fontsize=12)
+        
+        ax2=ax.twinx()
+        
+        # Set x-axis variable for right y-axis
+        ax2.scatter(df["time (min).3"]*0.5,
+                df ["MW [Da]"],
+                color="black",
+                s = 0.2)
+        ax2.set_ylabel(ylabel = "MW [Da]",
+                color="black",
+                fontsize=12)
+        
+        # Make second axis log-scaled
+        ax2.set_yscale("log")
+        ax2.set_ylim (1e4, 1e6)
+        
+        # x-axis
+        ax.set_xlabel("Volume [ml]", fontsize = 12)
+        if min_x and max_x:
+            ax.set_xlim(min_x, max_x)
+        ax.xaxis.set_major_locator(ticker.MultipleLocator(5))
+        ax.xaxis.set_major_formatter('{x:.0f}')
+        ax.xaxis.set_minor_locator(ticker.MultipleLocator(1))
+        
+        plt.title(sample_name)
+        if save_pdf:
+            plt.savefig(csv_path[:-4]+'.pdf')
+        if save_png: 
+            plt.savefig(csv_path[:-4]+'.png', dpi=300)
+        
+        plt.show()
+        plt.close('all')
+
     return None
