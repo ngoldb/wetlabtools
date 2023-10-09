4e8f9318fa6965f01682e5a536ebb03b82c0042c
Nicolas <nicolas.goldbach@epfl.ch>
2023-10-09 12:44:25 +0200

    bugfix for RUmax normalization

--- wetlabtools/utils.py
+++ wetlabtools/utils.py
@@ -4,7 +4,7 @@ This module contains simple helper functions
 
 def normalize_percent_max(y: float, max_value: float, min_value: float):
     '''Function to normalize data to a scale 0 - 100% of data range'''
-    return (y - min_value) / (max_value - min_value)
+    return ((y - min_value) / (max_value - min_value)) * 100
 
 
 def rel_scale(y: float, max_value: float):
