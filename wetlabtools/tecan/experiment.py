3098b1b8b338d9e166100ae51af5df06301dc2fc
Nicolas <nicolas.goldbach@epfl.ch>
2025-08-09 20:03:09 +0200

    added 384 well plate support

--- wetlabtools/tecan/experiment.py
+++ wetlabtools/tecan/experiment.py
@@ -2,50 +2,63 @@
 Tecan module
 """
 
-import openpyxl
 from wetlabtools.tecan.parse import parse_header, parse_data
 from wetlabtools.tecan.plotting import plot_96_well_plate
 
 class experiment(object):
     """Base class for tecan experiments"""
 
-    def __init__(self, name: str, data: str):
+    def __init__(self, name: str, data: str, plate_format: int=96):
         """
         name: str, name of the experiment
         data: str, path to the tecan excel file
+        plate_format: int, number of wells of the microplate
         """
         self.name = name
         self.data_file = data
-        self.metadata, self.protocol = parse_header(self.data_file)
+        self.plate_format = plate_format
+        self.metadata, self.protocol = parse_header(self.data_file, plate_format)
         self.data = self.__parse_all_data()
     
     def __parse_all_data(self):
         all_data = {}
-        implemented_types = ['Absorbance']
-
-        if 'Kinetic' in self.protocol: 
-            kinetic = True
-        else:
-            kinetic = False
+        implemented_types = ['Absorbance', 'Fluorescence Top Reading', 'Luminescence']
 
         for action in self.protocol:
-            if action in implemented_types:
-                x = self.protocol[action]
+            skip = 0
+            if action not in implemented_types:
+                if action == 'Plate': pass
+                else: print(f"data type not implemented: {action}")\
+                
+            elif action == 'Absorbance' or action == 'Fluorescence Top Reading' or action == 'Luminescence':
 
-                if "Reference wavelength [nm]" in x:
+                if "Reference wavelength [nm]" in self.protocol[action]:
                     identifier = 'Difference'
                 else:
                     identifier = '<>'
+                    if 'plate_layout' in self.protocol['Plate']: skip = 1
 
                 subset= self.protocol[action]['Part of Plate']
 
-                data = parse_data(self.data_file, identifier=identifier, subset=subset)
+                try:
+                    label = self.protocol[action]['label']
+                except:
+                    label = 'value'
+
+                data = parse_data(
+                    self.data_file, 
+                    identifier=identifier, 
+                    subset=subset, 
+                    label=label, 
+                    skip=skip, 
+                    plate=self.plate_format
+                )
                 all_data[action] = data
         
         return all_data
     
 
-    def show_plate_data(self, data_id: str=None):
+    def show_plate_data(self, data_id: str=None, **kwargs):
         '''
         data_id: str, identifier of the data to display
 
@@ -53,13 +66,16 @@ class experiment(object):
         '''
 
         df = self.data[data_id]
-        fig, ax = plot_96_well_plate(df, title=data_id)
+        fig, ax = plot_96_well_plate(df, title=data_id, **kwargs)
 
         return fig, ax
 
     # getters
     def get_name(self):
         return self.name
+
+    def get_format(self):
+        return self.plate_format
     
     def get_metadata(self):
         return self.metadata
@@ -72,3 +88,23 @@ class experiment(object):
     
     def get_data(self):
         return self.data
+    
+
+class elisa(experiment):
+    """
+    Class for elisa measurements on the Tecan
+    """
+
+    def __init__(self, name: str, data: str, exp_layout: str):
+        super().__init__(name, data)
+        self.exp_layout = exp_layout
+        self.coating, self.samples, self.concentrations = self.parse_layout()
+
+    def parse_layout(self):
+        action = self.protocol[0]
+        subset= self.protocol[action]['Part of Plate']
+        coating = parse_data(self.exp_layout, "Coating", subset=subset)
+        samples = parse_data(self.exp_layout, "Samples", subset=subset)
+        concentrations = parse_data(self.exp_layout, "Concentrations", subset=subset)
+
+        return coating, samples, concentrations
--- wetlabtools/tecan/parse.py
+++ wetlabtools/tecan/parse.py
@@ -2,10 +2,11 @@
 This module contains code to parse data from the Tecan Excel sheets
 """
 
+import string
 import openpyxl
 import pandas as pd
 
-def parse_header(excel_sheet):
+def parse_header(excel_sheet, plate_format: int):
         
         def row2list(row):
             row_list = [cell.value for cell in row]
@@ -30,7 +31,16 @@ def parse_header(excel_sheet):
                 row = next(iterator)
                 row_list = row2list(row)
                 while row_list != []:
-                    actions[action][row_list[0]] = ' '.join([str(x) for x in row_list[1:]])
+                    label_name = row_list[0]
+                    label_value = ' '.join([str(x) for x in row_list[1:]])
+
+                    # plate area information might be stored in two rows because Tecan SUCKS!!!!!
+                    if label_name == 'Plate area' and label_value.endswith(';'):
+                        row = next(iterator)
+                        row_list = row2list(row)
+                        label_value = label_value + ' '.join([str(x) for x in row_list])
+
+                    actions[action][label_name] = label_value
                     row = next(iterator)
                     row_list = row2list(row)
 
@@ -41,7 +51,7 @@ def parse_header(excel_sheet):
                         if row_list != []:
                             if row_list[0] == '<>':
                                 subset = actions['Plate']['Plate area']
-                                actions[action]['plate_layout'] = parse_data(excel_sheet, identifier='<>', subset=subset).rename({'value':'sample_type'}, axis=1)
+                                actions[action]['plate_layout'] = parse_data(excel_sheet, identifier='<>', subset=subset, plate=plate_format).rename({'value':'sample_type'}, axis=1)
                                 # skip to end of plate layout
                                 while row_list != []:
                                     row = next(iterator)
@@ -93,33 +103,60 @@ def parse_header(excel_sheet):
         return meta_data, protocol
 
 
-def parse_data(file: str, identifier: str, subset: str='A1-H12'):
+def parse_data(file: str, identifier: str, subset: str='A1-H12', label: str='value', skip: int=0, plate: int=96):
     """
     file: str, path to the tecan excel file
     identifier: str, identifier to find data
     subset: str, region of the excel sheet to use to retrieve data
+    label: str, label of the data
+    skip: int, number of appearances of identifier to skip
+    plate: int, number of wells on plate
 
     Function to parse data from Tecan excel sheet
     """
     wb_obj = openpyxl.load_workbook(file)
     sheet_obj = wb_obj.active
 
+    IMPLEMENTED = [96, 384]
+    assert plate in IMPLEMENTED, f'plate format {plate} not implemented! {IMPLEMENTED}'
+
+    # helper dicts for different plates
+    usecols = {
+        96: 'B:M',
+        384: 'B:Y'
+    }
+    indices = {
+        96: list('ABCDEFGH'),
+        384: list('ABCDEFGHIJKLMNOP')
+    }
+    offsets = {
+        96: 8,
+        384: 16
+    }
+
+    count = 0
     for row in sheet_obj.iter_rows():
         cell = row[0]
-        if cell.value == identifier:
+        if cell.value == identifier and count == skip:
             if identifier != '<>':
+                # TODO: fix this if there is a plate map, which has sample names (like PC) in the way
                 start_row = cell.row + 1
             else:
                 start_row = cell.row
             
-            # we now that a 96 well plate has 8 rows
-            end_row = start_row + 8
+            # locating end row
+            end_row = start_row + offsets[plate]
             break
+
+        elif cell.value == identifier:
+            count += 1
 
     # read data from excel region
     skip = lambda x: x not in range(start_row - 1, end_row)
-    df = pd.read_excel(file, usecols='B:M', skiprows=skip)
-    df.index = list('ABCDEFGH')
+    df = pd.read_excel(file, usecols=usecols[plate], skiprows=skip)
+    index = indices[plate]
+    df.index = index
+    df.columns = df.columns.astype(str)
 
     # select subsets
     subsets = subset.split(';')
@@ -134,7 +171,7 @@ def parse_data(file: str, identifier: str, subset: str='A1-H12'):
 
         df_sub = df.loc[start_row : end_row, start_col : end_col]
         df_sub = df_sub.reset_index()
-        df_melted = pd.melt(df_sub, id_vars='index', var_name='column', value_name='value')
+        df_melted = pd.melt(df_sub, id_vars='index', var_name='column', value_name=label)
         df_melted['well'] = df_melted['index'] + df_melted['column'].astype(str)
         df_melted.drop(['index', 'column'], axis=1, inplace=True)
         
@@ -142,3 +179,46 @@ def parse_data(file: str, identifier: str, subset: str='A1-H12'):
     
     data.drop_duplicates(subset='well', inplace=True)
     return data
+
+
+def parse_wavel_scan(file: str, identifier: str, n_wavelengths: int, plate_region: str='A1-H12', label: str='value'):
+    """
+    file: str, path to the tecan excel file
+    identifier: str, identifier to find data
+    n_wavelengths: int, number of wavelengths measured
+    plate_region: str, region of the plate with samples
+    label: str, name of the measured variable
+
+    Function to parse wavelength scan data from Tecan excel files
+    """
+    wb_obj = openpyxl.load_workbook(file)
+    sheet_obj = wb_obj.active
+    for row in sheet_obj.iter_rows():
+        cell = row[0]
+        if cell.value == identifier:
+            excel_start_row = cell.row
+            excel_end_row = int(excel_start_row + n_wavelengths)
+            break
+
+    subsets = plate_region.split(';')
+    all_wells = []
+    for subset in subsets:
+        start, end = subset.split('-')
+        start_row = start[0]
+        end_row = end[0]
+        start_col = start[1:]
+        end_col = end[1:]
+
+        rows = [chr(i) for i in range(ord(start_row),ord(end_row)+1)]
+        cols = [i for i in range(int(start_col), int(end_col) + 1)]
+
+        for x in cols:
+            for y in rows:
+                all_wells.append(y + str(x))
+    
+    end_col_letter = string.ascii_uppercase[len(all_wells)]
+    skip = lambda x: x not in range(excel_start_row - 1, excel_end_row + 1)
+    df = pd.read_excel(file, usecols=f'A:{end_col_letter}', skiprows=skip)
+    df_melted = pd.melt(df, ignore_index=True, id_vars="Wavel.", var_name='Well', value_name=label)
+
+    return df_melted
